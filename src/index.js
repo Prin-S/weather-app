05ae7bf95e9dd7c73c0507bd0b31c7b4d212da56
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-09-05 21:37:14 +0700

    Modified getWeather(), added elements to display weather data, and added CSS.
    - Chose weather data to display on the front end.
    - getWeather() shows the weather data using CSS grid.
    - The units change depending on whether °C or °F is selected.

--- src/index.js
+++ src/index.js
@@ -7,26 +7,60 @@ const weatherSubmit = document.querySelector('#weather-form');
 weatherSubmit.addEventListener('submit', getWeather);
 
 const message = document.querySelector('#message');
+let selectedWeather = {};
+
+const wrapper = document.querySelector('.wrapper');
+const box1 = document.querySelector('#box1-text');
+const box2 = document.querySelector('#box2-text');
+const box3 = document.querySelector('#box3-text');
+const box4 = document.querySelector('#box4-text');
+const box5 = document.querySelector('#box5-text');
+const box6 = document.querySelector('#box6-text');
 
 function getWeather(event) {
-    const unit = document.querySelector('input[name=unit]:checked');
     message.innerHTML = '';
+    const unit = document.querySelector('input[name=unit]:checked');
+    let degreeText = '';
+    let rainText = '';
+
+    if (unit.value == 'metric') {
+        degreeText = '°C';
+        rainText = 'mm';
+    } else {
+        degreeText = '°F';
+        rainText = 'in';
+    }
 
     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location.value}?unitGroup=${unit.value}&key=${visualCrossingAPI.value}`, {mode: 'cors'})
         .then(function(response) {
             return response.json();
         })
         .catch(function(err) {
-            message.innerHTML = err;
+            message.innerHTML = 'Location not found';
         })
         .then(function(response) {
-            console.log(response);
+            selectedWeather = {
+                location: response.resolvedAddress,
+                temp: response.currentConditions.temp,
+                conditions: response.currentConditions.conditions,
+                humidity: response.currentConditions.humidity,
+                feelsLike: response.currentConditions.feelslike,
+                rain: response.currentConditions.precip,
+                rainProb: response.currentConditions.precipprob,
+                weatherDesc: response.description,
+            };
+
+            wrapper.style.display = 'grid';
+            box1.innerHTML = `<div><p>${selectedWeather.location}</p><h2>${selectedWeather.temp}${degreeText}</h2><p>${selectedWeather.conditions}</p></div>`;
+            box2.innerHTML = `<h3>${selectedWeather.humidity}%</h3>`;
+            box3.innerHTML = `<h3>${selectedWeather.feelsLike}${degreeText}</h3>`;
+            box4.innerHTML = `<h3>${selectedWeather.rain} ${rainText}</h3>`;
+            box5.innerHTML = `<h3>${selectedWeather.rainProb}%</h3>`;
+            box6.innerHTML = `<p>${selectedWeather.weatherDesc}</p>`;
         })
         .catch(function(err) {
             console.log(err);
-        })
+        });
 
     event.preventDefault();
 }
-
-console.log('Hello! Hello!');
